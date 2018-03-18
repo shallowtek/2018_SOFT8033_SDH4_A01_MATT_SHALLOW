0225cf2338853ac9c975ff1f70143ddad5745f0a
Matt Shallow <mattshallow@gmail.com>
2018-03-18 10:50:50 +0000

    experimental changes hint 3

--- Hint-3/A01 - Hint3.py
+++ Hint-3/A01 - Hint3.py
@@ -16,29 +16,36 @@ import codecs
 # ------------------------------------------
 # FUNCTION process_line
 # ------------------------------------------
-def process_line(line):
-    # 1. We create the output variable
-    res = []
 
-    # 2. We clean the line
-    line = line.replace('\t', ' ')
-    line = line.replace('  ', ' ')
-    line = line.replace('   ', ' ')
-    line = line.replace('    ', ' ')
+# def process_line(line, language):
+#     # 1. We create the output variable
+#     res = []
 
-    line = line.strip()
-    line = line.rstrip()
+#     # 2. We clean the line
+#     line = line.replace('\t', ' ')
+#     line = line.replace('  ', ' ')
+#     line = line.replace('   ', ' ')
+#     line = line.replace('    ', ' ')
 
-    # 3. We split the line by words
-    words = line.split(" ")
+#     line = line.strip()
+#     line = line.rstrip()
 
-    # 4. We append each words to the list
-    for word in words:
-        if word != '':
-            res.append(word)
+#     # 3. We split the line by words
+#     words = line.split(" ")
+#     first_word = words[0]
+    
+#     if "." in first_word:
+#       lang, project = first_word.split(".", 1)
+#     else:
+#       lang = first_word
+#     # 4. We append each words to the list
+    
+    
+#     if lang in languages:
+       
 
-    # 5. We return res
-    return res
+#     # 5. We return res
+#     return res
 
 # ------------------------------------------
 # FUNCTION filter languages
@@ -74,18 +81,53 @@ def filter_languages(line, languages):
     # 5. We return res
     return res
 # ------------------------------------------
+# Top Five
+# ------------------------------------------
+def top_five(line):
+#     current_lang = ""
+    #next_lang = line[0]
+    
+#     temp_list = []
+    overall_list = []    
+  
+#     if next_lang != current_lang:
+      
+#       if current_lang != "":
+#         temp_list.sort(key = lambda x: x[2])
+#         temp_list[:5]
+#         overall_list.extend(temp_list)
+#         temp_list[:]
+          
+#       current_lang = next_lang
+#       temp_list.append(line)
+      
+    overall_list.append[line]
+    
+    return overall_list
+# ------------------------------------------
 # FUNCTION my_main
 # ------------------------------------------
 def my_main(dataset_dir, o_file_dir, languages, num_top_entries):
     inputRDD = sc.textFile(dataset_dir)
-    sampleData = inputRDD.sample(False,0.001)
+    sample_data = inputRDD.sample(False,0.001)
+    filter_lang = sample_data.filter(lambda x: filter_languages(x, languages))
+    sorted_lang = filter_lang.sortBy(lambda x: x)
+    
+    
+   
+   # split_words = sorted_lang.map(lambda x: x.split(" "))
+    
+    #filter_views = split_words.map(lambda x: top_five(x))
+    #sorted_views = split_words.sortBy(lambda x: x[2], False)
     # 2. We split the dataset by words
     #all_wordsRDD = sampleData.map(lambda x: process_line(x))
-    filtered_sample = sampleData.filter(lambda x: filter_languages(x, languages))
-    sorted_sample = filtered_sample.sortBy(lambda x: x)
+    #filtered_sample = sampleData.filter(lambda x: filter_languages(x, languages))
+    #sorted_sample = filtered_sample.sortBy(lambda x: x)
+    #words_sample = sorted_sample.map(lambda x: x.split(" "))
+   #sorted_sample_two = words_sample.sortBy(lambda x: x[2], False)
     #filtered_langs = sampleData.filter(lambda x: filter_languages(x, languages))
     #filtered_langs = all_wordsRDD.filter(lambda x: filter_languages(x, languages))    
-    for f in sorted_sample.take(20):      
+    for f in sorted_lang.take(20):      
       print(f)
 
 # ---------------------------------------------------------------
